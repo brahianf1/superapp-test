c16b8a45a83dd287f72bf19f9da1ffc6ddf44768
brahianf1 <floresbrahian@gmail.com>
2024-10-28 19:38:20 -0300

    first commit

---
+++ src/test/java/me/brahian/superapp/SuperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brahian.superapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
